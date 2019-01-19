f555e073a983d799c9fd8a806f1de7bc09038742
Sam <Mr.Jason_Sam@iCloud.com>
2019-01-20 01:33:30 +0800

    thread frame

---
+++ core/mqosfactory.cpp
@@ -0,0 +1,6 @@
+#include "mqosfactory.h"
+
+MQOSFactory::MQOSFactory()
+{
+
+}
---
+++ core/mqosfactory.h
@@ -0,0 +1,11 @@
+#ifndef MQOSFACTORY_H
+#define MQOSFACTORY_H
+
+
+class MQOSFactory
+{
+public:
+    MQOSFactory();
+};
+
+#endif // MQOSFACTORY_H
